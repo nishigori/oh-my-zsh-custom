c07e5d08e19175b1dc15692ee0f3bee455056add
Takuya Nishigori <nishigori.tak@gmail.com>
2012-04-25 20:51:29 +0900

    Change the run-selenium command alias.

--- alias.zsh
+++ alias.zsh
@@ -23,5 +23,5 @@ alias vless='vim -R'
 alias gitlog='git log --graph --color --pretty="%x09%h %cn%x09%s %Cred%d"'
 
 # Selenium RC
-alias start_selenium='java -jar /usr/local/bin/selenium-server.jar'
+alias run_selenium='java -jar /usr/local/bin/selenium-server.jar'
 

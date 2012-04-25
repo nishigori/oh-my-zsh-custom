93948d1db6a224e2e4d953ddf200ec5ed09266ed
Takuya Nishigori <nishigori.tak@gmail.com>
2012-04-25 21:38:52 +0900

    Removed local dependency aliases.

--- alias.zsh
+++ alias.zsh
@@ -1,9 +1,6 @@
 # expand aliases before completing
 setopt complete_aliases     # aliased ls needs if file/dir completions work
 
-alias where="command -v"
-alias j="jobs -l"
-
 alias l='ls -l'
 alias la='ls -al'
 alias lt='ls -lt'
@@ -21,7 +18,4 @@ alias vless='vim -R'
 # git
 #alias gitlog='git log --graph --all --color --pretty="%x09%h %cn%x09%s %Cred%d"'
 alias gitlog='git log --graph --color --pretty="%x09%h %cn%x09%s %Cred%d"'
-
-# Selenium RC
-alias run_selenium='java -jar /usr/local/bin/selenium-server.jar'
 

4563d40b7f0f0a95fd63445339ba586b1e066adf
Takuya Nishigori <nishigori.tak@gmail.com>
2012-05-06 03:11:57 +0900

    Updated PHP ver 5.4

--- OS/mac.zsh
+++ OS/mac.zsh
@@ -2,7 +2,8 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 #homebrew & PHP PEAR Packages
-export PATH="$(brew --prefix)/Cellar/php/5.3.8/bin:$(brew --prefix)/bin:$PATH"
+#export PATH="/usr/local/php5/bin:$(brew --prefix)/Cellar/php/5.3.8/bin:$(brew --prefix)/bin:$PATH"
+export PATH="$(brew --prefix)/Cellar/php/5.4.0/bin:$(brew --prefix)/bin:/usr/local/php5/bin:$PATH"
 
 # MacVim Kaoriya
 alias gvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -g "$@"'

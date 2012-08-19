b848ff9806d802d468096c859cccdf9e28185488
Takuya Nishigori <nishigori.tak@gmail.com>
2012-08-19 19:28:40 +0900

    Change mac php setting

--- OS/mac.zsh
+++ OS/mac.zsh
@@ -3,7 +3,7 @@
 
 #homebrew & PHP PEAR Packages
 #export PATH="/usr/local/php5/bin:$(brew --prefix)/Cellar/php/5.3.8/bin:$(brew --prefix)/bin:$PATH"
-export PATH="$(brew --prefix)/Cellar/php/5.4.0/bin:$(brew --prefix)/bin:/usr/local/php5/bin:$PATH"
+#export PATH="$(brew --prefix)/Cellar/php/5.4.0/bin:$(brew --prefix)/bin:/usr/local/php5/bin:$PATH"
 
 # MacVim Kaoriya
 alias gvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -g "$@"'
@@ -12,8 +12,8 @@ alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"
 
 # ctags
 # ctag config file is .ctags
+alias ctags="`brew --prefix`/bin/ctags"
 alias ctag='ctags -R ./'
-alias ctags='ctags -R'
 
 # make vimperator xpi
 alias makevimp='cd /usr/local/src/vimperator-labs; make -e SED=gsed CP=gcp -C vimperator xpi; open /usr/local/src/vimperator-labs/downloads/'
@@ -23,3 +23,5 @@ alias makevimp='cd /usr/local/src/vimperator-labs; make -e SED=gsed CP=gcp -C vi
 # FIXME: htmlファイル作ってまうでよ!!
 #export LESS='-R'
 #export LESSOPEN='| $(brew --prefix)/bin/src-hilite-lesspipe %s'
+
+#export PATH="/usr/local/Cellar/php54/5.4.4/bin/phpunit"

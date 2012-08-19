3722767ea9ea71cb0b428da606549c5b7d647b6f
Takuya Nishigori <nishigori.tak@gmail.com>
2012-08-20 03:38:43 +0900

    Fixed invalid path

--- local.zsh
+++ local.zsh
@@ -1,4 +1,4 @@
 # Loading local dependency
-find ./local -type f -name '*.zsh' | while read file; do
+find $HOME/.oh-my-zsh/custom/local -type f -name '*.zsh' | while read file; do
   source $file
 done

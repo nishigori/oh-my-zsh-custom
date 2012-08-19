b5957be332fbbb6683cebaa706de28618e71fcb2
Takuya Nishigori <nishigori.tak@gmail.com>
2012-08-19 19:44:13 +0900

    Add quoute previous word recipe

---
+++ quote-previous-word.zsh
@@ -0,0 +1,19 @@
+# INFO: 一つ前の単語をある文字列で加工
+#       ' は<A-s>|<M-s>
+#       " は<A-d>|<M-d>
+
+# quote previous word in single or double quote
+autoload -U modify-current-argument
+_quote-previous-word-in-single() {
+    modify-current-argument '${(qq)${(Q)ARG}}'
+    zle vi-forward-blank-word
+}
+zle -N _quote-previous-word-in-single
+bindkey '^[s' _quote-previous-word-in-single
+
+_quote-previous-word-in-double() {
+    modify-current-argument '${(qqq)${(Q)ARG}}'
+    zle vi-forward-blank-word
+}
+zle -N _quote-previous-word-in-double
+bindkey '^[d' _quote-previous-word-in-double
